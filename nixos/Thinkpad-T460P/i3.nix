554b5a85d4cf2f5f8a313db790d37bdf1395d472
LER0ever <etasry@gmail.com>
2017-03-04 15:50:24 -0600

    update config for i3

--- nixos/Thinkpad-T460P/i3.nix
+++ nixos/Thinkpad-T460P/i3.nix
@@ -1,35 +1,37 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./fonts.nix
-    ];
+	imports =
+		[ # Include the results of the hardware scan.
+		./fonts.nix
+		];
 
-  environment.systemPackages = with pkgs; [
-     neofetch
-     rcm
-     rofi
-     i3blocks
-     font-awesome-ttf
-     conky
-     terminator
-     nitrogen
+	environment.systemPackages = with pkgs; [
+		neofetch
+		rcm
+		rofi
+		i3blocks
+		font-awesome-ttf
+		conky
+		terminator
+		pcmanfm
+		nitrogen
+		networkmanagerapplet
 
-     # i3blocks requirements
-     bc
-     wirelesstools
-  ];
+# i3blocks requirements
+		bc
+		wirelesstools
+	];
 
-  services =
-  {
-    xserver =
-    {
-      enable = true;
-      windowManager.i3.enable = true;
-      windowManager.i3.package = pkgs.i3-gaps;
-      desktopManager.xterm.enable = false;
-    };
-  };
+	services =
+	{
+		xserver =
+		{
+			enable = true;
+			windowManager.i3.enable = true;
+			windowManager.i3.package = pkgs.i3-gaps;
+			desktopManager.xterm.enable = false;
+		};
+	};
 
 }
--- nixos/Thinkpad-T460P/kde5.nix
+++ nixos/Thinkpad-T460P/kde5.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ...}:
 {
   services.xserver = {
-    displayManager.sddm.enable = true;
+    displayManager.lightdm.enable = true;
     desktopManager.plasma5 = {
       enable = true;
     };
